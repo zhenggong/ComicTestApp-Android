1c1d99eb0296dd0d3a375f430ca8627be4f4c0ac
Zheng Gong <zheng.gong@g-pc-5402.intra.gree-office.net>
2018-04-18 11:19:54 +0900

    add welcome activity

--- app/src/main/java/glossom/comictestapp_android/WelcomeActivity.java
+++ app/src/main/java/glossom/comictestapp_android/WelcomeActivity.java
@@ -1,4 +1,17 @@
 package glossom.comictestapp_android;
 
-public class WelcomeActivity {
+import android.view.animation.Animation;
+
+import com.king.base.SplashActivity;
+
+public class WelcomeActivity extends SplashActivity{
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_welcome;
+    }
+
+    @Override
+    public Animation.AnimationListener getAnimationListener() {
+        return null;
+    }
 }
